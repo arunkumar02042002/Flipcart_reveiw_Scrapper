745b9f53a73ff2f3b21fee4a4e79629f360255e5
arunkumar02042002 <84587079+arunkumar02042002@users.noreply.github.com>
2023-05-03 15:33:42 +0530

    Final Commit 2

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# Web_scrapper_integration_with_restapi_mongodb_and_webportal
+# Flipkart Review Scraper
+This project is a web scraper that extracts product reviews from Flipkart, an Indian e-commerce website, using Python and Flask web framework. The extracted data includes the product name, rating, review title, and review comments.
+
+The project is designed as a web application that takes user input as a search key and returns the reviews for the corresponding product. It uses Flask routes to handle requests from the user and Beautiful Soup library to extract data from the Flipkart website. The extracted data is then stored in a MongoDB database and a CSV file.
+
+# Getting Started
+To run this project locally, you need to have Python installed on your system. You can follow these steps:
+
+1. Clone this repository on your local machine
+2. Install the required Python modules using the following command: pip install -r requirements.txt
+3. Run the Flask application using the following command: python app.py
+4. Open your browser and go to http://localhost:5000 to access the application.
+
+# How to use
+To use this application, follow these steps:
+
+1. Enter the search key for the product you want to extract reviews for.
+2. Click on the "Search" button.
+3. The application will extract the reviews for the corresponding product and display them on the results page.
+4. Technologies Used
+
+# This project uses the following technologies:
+- Python
+- Flask
+- Beautiful Soup
+- MongoDB
+- CSV
+
+# Acknowledgements
+This project was inspired by web scraping tutorials of PW Skills team. Special thanks to Flask and Beautiful Soup developers for creating such great tools!
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Importing Necessary Modules
 from flask import Flask, render_template, request,jsonify
 from flask_cors import CORS,cross_origin
 import requests
@@ -6,27 +7,39 @@ from urllib.request import urlopen as uReq
 import logging
 import pymongo
 import csv
+
+# Logging informations to scrapper.log
 logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
 
+# Creating a flask object
 app = Flask(__name__)
 
+# Setting route to home page
 @app.route("/", methods = ['GET'])
 def homepage():
     return render_template("index.html")
 
+# Setting route to review page
 @app.route("/review" , methods = ['POST' , 'GET'])
 def index():
     if request.method == 'POST':
         try:
             search_key = request.form['content'].replace(" ","")
+            # Generating the link to the filpkart page for the specific search key
             flipkart_url = "https://www.flipkart.com/search?q=" + search_key
             uClient = uReq(flipkart_url)
+
+            # Reading the webpage
             flipkartPage = uClient.read()
             uClient.close()
             flipkart_html = bs(flipkartPage, "html.parser")
             bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
+
+            # Deleting unecessory captures
             del bigboxes[0:3]
             box = bigboxes[0]
+
+            # Getting product link
             productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
             prodRes = requests.get(productLink)
             prodRes.encoding='utf-8'
@@ -34,36 +47,41 @@ def index():
             print(prod_html)
             commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
 
+            # Scrapping name, rating, review and comments
             reviews = []
             for commentbox in commentboxes:
+                # Name
                 try:
                     name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
-
                 except:
                     logging.info("name")
 
+                # Rating
                 try:
                     rating = commentbox.div.div.div.div.text
-
-
                 except:
                     rating = 'No Rating'
                     logging.info("rating")
 
+                # Review
                 try:
                     commentHead = commentbox.div.div.div.p.text
-
                 except:
                     commentHead = 'No Comment Heading'
                     logging.info(commentHead)
+
+                # Comment
                 try:
                     comtag = commentbox.div.div.find_all('div', {'class': ''})
                     custComment = comtag[0].div.text
                 except Exception as e:
                     logging.info(e)
-
+                
+                # Creating a dictionary with captured details
                 mydict = {"Product": search_key, "Name": name, "Rating": rating, "CommentHead": commentHead,
                           "Comment": custComment}
+
+                # Storing the dictionary into the list
                 reviews.append(mydict)
             logging.info("log my final result {}".format(reviews))
 
@@ -89,6 +107,7 @@ def index():
                 dict_writer.writeheader()
                 dict_writer.writerows(reviews)
 
+            # Returning the result template
             return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
 
         except Exception as e:
@@ -97,7 +116,6 @@ def index():
 
     else:
         return render_template('index.html')
-
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
